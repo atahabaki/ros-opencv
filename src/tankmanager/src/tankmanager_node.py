ed2364e49b0db5350d92c6f04f05283d30fae64a
A. Taha Baki <atahabaki@pop-os.localdomain>
2020-09-04 14:35:31 +0300

    tankmanager servo fix

--- src/tankmanager/src/tankmanager_node.py
+++ src/tankmanager/src/tankmanager_node.py
@@ -9,8 +9,7 @@ import navio.util
 class TankManager:
     _NODE_NAME="tankmanager"
     _NODE_STATUS="tankmanager/status"
-    def __init__(self,rc_channel=6,min_pwm=.750,max_pwm=1.350):
-        self.status = False # Status => False =: Cover is already closed, True =: Cover is already open
+    def __init__(self,rc_channel=4,min_pwm=1.250,max_pwm=1.750):
         self.rc_channel=rc_channel
         self.min_pwm=min_pwm
         self.max_pwm=max_pwm
@@ -19,7 +18,6 @@ class TankManager:
         self.pub_stat = rospy.Publisher(self._NODE_STATUS, String, queue_size=10)
         self.rate = rospy.Rate(60)
         #If apm is running kill this process
-        navio.util.check_apm()
 
     def task_finished(self):
         self.pub_stat.publish("finished")
@@ -27,42 +25,53 @@ class TankManager:
     def task_failed(self):
         self.pub_stat.publish("failed")
 
-    def changeStatus(self,status=False):
-        self.status=status
-
-    def openCover(self):
-        print("Opening")
+    def openCover(self,atime=1):
+        rospy.loginfo("opening cover")
         with navio.pwm.PWM(self.rc_channel) as pwm:
             pwm.set_period(50)
             pwm.enable()
-            pwm.set_duty_cycle(self.max_pwm)
-        print("Opened")
-        self.changeStatus(status=True)
+            start=time.time()
+            while(True):
+                now=time.time()
+                dist=now-start
+                if dist >= atime:
+                    break
+                pwm.set_duty_cycle(self.max_pwm)
 
-    def closeCover(self):
-        print("Closing")
+    def closeCover(self,atime=1):
+        rospy.loginfo("closing cover")
         with navio.pwm.PWM(self.rc_channel) as pwm:
             pwm.set_period(50)
             pwm.enable()
-            pwm.set_duty_cycle(self.min_pwm)
-        print("Closed")
-        self.changeStatus()
+            start=time.time()
+            while(True):
+                now=time.time()
+                dist=now-start
+                if dist >= atime:
+                    break
+                pwm.set_duty_cycle(self.min_pwm)
+
+    def sleep(self,sleep_time=5):
+        i=0
+        while(True):
+            if i >= sleep_time:
+                break
+            time.sleep(1)
+            i+=1
+            rospy.loginfo("slept {} second(s)".format(i))
 
     def openAndClose(self,sleep_time=5):
         self.openCover()
-        self.changeStatus(status=True)
-        time.sleep(sleep_time)
+        self.sleep()
         self.closeCover()
-        self.changeStatus()
 
-    def toggle_cover(self):
-        if self.status == True:
-            self.closeCover()
-        else:
-            self.openCover()
+    def closeAndOpen(self,sleep_time=5):
+        self.closeCover()
+        self.sleep()
+        self.openCover()
 
 def main():
-    tankmngr = TankManager()
+    tankmngr = TankManager(4)
     try:
         tankmngr.openAndClose(5)
     except:
