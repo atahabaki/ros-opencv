ea77efb4e0053c4143da24609f0ae4b0c86e2e1d
A. Taha Baki <atahabaki@pop-os.localdomain>
2020-09-12 19:26:15 +0300

    easyctrl

--- src/philosopher/src/easyctrl.py
+++ src/philosopher/src/easyctrl.py
@@ -17,6 +17,7 @@ class EasyControl:
         self.tankmanager_change_name = "tankmanager/status/change"
         self.tankmanager_status_name = "tankmanager/status"
         self.finder_srv_name = "/finder/where"
+        self.mavros_state_name = "/mavros/state"
 
     #######################################
     # Arming
@@ -66,8 +67,57 @@ class EasyControl:
     # Mode changing
     #######################################
 
+    def __mode(self,mode_name="GUIDED",base_mode=0):
+        self.mavros_mode_clnt = rospy.ServiceProxy(self.change_mode_srv_name,SetMode)
+        self.mavros_mode_clnt(base_mode=base_mode,custom_mode=mode_name)
+
+    def change2Guided(self):
+        rospy.wait_for_service(self.change_mode_srv_name)
+        try:
+            self.__mode()
+        except:
+            rospy.logerr("Set mode \"GUIDED\" failed.")
+
+    def change2Stabilize(self):
+        rospy.wait_for_service(self.change_mode_srv_name)
+        try:
+            self.__mode(mode_name="STABILIZE")
+        except:
+            rospy.logerr("Set mode \"STABILIZE\" failed.")
+
+    
     #######################################
-    # State managing
+    # Landing Client
+    #######################################
+    
+    def __land(self):
+        self.landing_clnt = rospy.ServiceProxy(self.landing_srv_name,CommandTOL)
+        self.landing_clnt(altitude=0,latitude=0,longtitude=0,min_pitch=0,yaw=0)
+
+    def land(self):
+        rospy.wait_for_service(self.landing_srv_name)
+        try:
+            self.__land()
+        except:
+            rospy.logerr("Landing failed.")
+
+    #######################################
+    # Takeoff Client
+    #######################################
+    
+    def __takeoff(self,_altitude):
+        self.landing_clnt = rospy.ServiceProxy(self.takeoff_srv_name,CommandTOL)
+        self.landing_clnt(altitude=_altitude,latitude=0,longtitude=0,min_pitch=0,yaw=0)
+
+    def takeoff(self,altitude=6):
+        rospy.wait_for_service(self.landing_srv_name)
+        try:
+            self.__takeoff(_altitude=altitude)
+        except:
+            rospy.logerr("Takeoff failed.")
+
+    #######################################
+    # State Tests
     #######################################
 
     #######################################
--- src/philosopher/src/philosopher_node.py
+++ src/philosopher/src/philosopher_node.py
@@ -4,3 +4,41 @@ import time
 from easyctrl import EasyControl
 #from finder.msg import Where
 
+def menu():
+    print("""
+1) Arm
+2) Disarm
+3) Change mode to GUIDED
+4) Change mode to STABILIZE
+5) Takeoff
+6) Land
+""")
+    x = input("Command (1-6): ")
+    x = x.strip()
+    return x
+
+def main():
+    easyctrl = EasyControl()
+    rospy.init_node("philosopher")
+    while not rospy.is_shutdown():
+        x = menu()
+        if x == "1":
+            easyctrl.arm()
+        elif x == "2":
+            easyctrl.disarm()
+        elif x == "3":
+            easyctrl.change2Guided()
+        elif x == "4":
+            easyctrl.change2Stabilize()
+        elif x == "5":
+            easyctrl.takeoff()
+        elif x == "6":
+            easyctrl.land()
+        else:
+            exit()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        rospy.logerr("Exception occured while main...")
