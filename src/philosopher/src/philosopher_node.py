01384c4af4b9c61d934e3716e70e396612aa1ab4
A. Taha Baki <atahabaki@pop-os.localdomain>
2020-09-12 19:56:13 +0300

    -easyctrl

--- src/philosopher/src/philosopher_node.py
+++ src/philosopher/src/philosopher_node.py
@@ -1,8 +1,132 @@
 #!/usr/bin/env python
 import rospy
 import time
-from easyctrl import EasyControl
+from finder.srv import *
+from mavros_msgs.msg import *
+from mavros_msgs.srv import *
+#from philosopher import EasyControl
 #from finder.msg import Where
+
+class Philosopher:
+    def __init__(self):
+        self._INT_LIMIT=5000
+        self._FLOAT_LIMIT=5000
+        self.arming_srv_name = "/mavros/cmd/arming"
+        self.takeoff_srv_name = "/mavros/cmd/takeoff"
+        self.landing_srv_name = "/mavros/cmd/land"
+        self.change_mode_srv_name = "/mavros/set_mode"
+        self.tankmanager_change_name = "tankmanager/status/change"
+        self.tankmanager_status_name = "tankmanager/status"
+        self.finder_srv_name = "/finder/where"
+        self.mavros_state_name = "/mavros/state"
+
+    def init(self):
+        rospy.init_node("philosopher")
+
+    #######################################
+    # Arming
+    #######################################
+
+    def __arm(self,data=True):
+        self.arming_clnt = rospy.ServiceProxy(self.arming_srv_name,CommandBool)
+        self.arming_clnt(data)
+
+    def arm(self):
+        rospy.wait_for_service(self.arming_srv_name)
+        try:
+            self.__arm()
+        except:
+            rospy.logerr("Arming failed.")
+
+    def disarm(self):
+        rospy.wait_for_service(self.arming_srv_name)
+        try:
+            self.__arm(data=False)
+        except:
+            rospy.logerr("Disarming failed.")
+
+    #######################################
+    # Servo Ctrl
+    #######################################
+
+    def __pwm(self,data="open"):
+        self.tankmanager_change_pub = rospy.Publisher(self.tankmanager_change_name, String, queue_size=10)
+        self.tankmanager_change_pub.publish(data)
+
+    def open_cover(self):
+        rospy.wait_for_message(self.tankmanager_status_name,String)
+        try:
+            self.__pwm()
+        except:
+            rospy.logerr("Open cover failed.")
+
+    def close_cover(self):
+        rospy.wait_for_message(self.tankmanager_status_name,String)
+        try:
+            self.__pwm()
+        except:
+            rospy.logerr("Close cover failed.")
+
+    #######################################
+    # Mode changing
+    #######################################
+
+    def __mode(self,mode_name="GUIDED",base_mode=0):
+        self.mavros_mode_clnt = rospy.ServiceProxy(self.change_mode_srv_name,SetMode)
+        self.mavros_mode_clnt(base_mode=base_mode,custom_mode=mode_name)
+
+    def change2Guided(self):
+        rospy.wait_for_service(self.change_mode_srv_name)
+        try:
+            self.__mode()
+        except:
+            rospy.logerr("Set mode \"GUIDED\" failed.")
+
+    def change2Stabilize(self):
+        rospy.wait_for_service(self.change_mode_srv_name)
+        try:
+            self.__mode(mode_name="STABILIZE")
+        except:
+            rospy.logerr("Set mode \"STABILIZE\" failed.")
+
+    
+    #######################################
+    # Landing Client
+    #######################################
+    
+    def __land(self):
+        self.landing_clnt = rospy.ServiceProxy(self.landing_srv_name,CommandTOL)
+        self.landing_clnt(altitude=0,latitude=0,longtitude=0,min_pitch=0,yaw=0)
+
+    def land(self):
+        rospy.wait_for_service(self.landing_srv_name)
+        try:
+            self.__land()
+        except:
+            rospy.logerr("Landing failed.")
+
+    #######################################
+    # Takeoff Client
+    #######################################
+    
+    def __takeoff(self,_altitude):
+        self.landing_clnt = rospy.ServiceProxy(self.takeoff_srv_name,CommandTOL)
+        self.landing_clnt(altitude=_altitude,latitude=0,longtitude=0,min_pitch=0,yaw=0)
+
+    def takeoff(self,altitude=6):
+        rospy.wait_for_service(self.landing_srv_name)
+        try:
+            self.__takeoff(_altitude=altitude)
+        except:
+            rospy.logerr("Takeoff failed.")
+
+    #######################################
+    # State Tests
+    #######################################
+
+    #######################################
+    # Drone Control
+    #######################################
 
 def menu():
     print("""
@@ -18,22 +142,21 @@ def menu():
     return x
 
 def main():
-    easyctrl = EasyControl()
-    rospy.init_node("philosopher")
+    philosopher = Philosopher()
     while not rospy.is_shutdown():
         x = menu()
         if x == "1":
-            easyctrl.arm()
+            philosopher.arm()
         elif x == "2":
-            easyctrl.disarm()
+            philosopher.disarm()
         elif x == "3":
-            easyctrl.change2Guided()
+            philosopher.change2Guided()
         elif x == "4":
-            easyctrl.change2Stabilize()
+            philosopher.change2Stabilize()
         elif x == "5":
-            easyctrl.takeoff()
+            philosopher.takeoff()
         elif x == "6":
-            easyctrl.land()
+            philosopher.land()
         else:
             exit()
 
